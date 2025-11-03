4657130387ba0093a9f0cd37dbd1eac6170dfcfd
Bhoomi Agarwal <bhoomiagarwal187@gmail.com>
2025-11-04 00:04:46 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-const { PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
-
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.17",
   networks: {
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [PRIVATE_KEY],
+    coreTestnet: {
+      url: "https://rpc.test2.btcs.network",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1114,
     },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
   },
 };
